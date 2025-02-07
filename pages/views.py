854b7545653df5c21015a084815d581720a2d73c
EROL KILBÜKER <erolposta@gmail.com>
2025-02-07 14:06:57 +0300

    Add pages app to settings, update index view and template, and create layout template

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import TemplateDoesNotExist
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Anasayfa")
+    try:
+        return render(request, 'index.html')
+    except TemplateDoesNotExist:
+        return HttpResponse("Template not found")
+    #return HttpResponse("index")
 
 def iletisim(request):
     return HttpResponse("İletişim")
