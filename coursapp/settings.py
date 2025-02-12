f574f2ddaecc49a38449fcc334796d5ebc3e9f52
EROL KILBÜKER <erolposta@gmail.com>
2025-02-12 15:05:23 +0300

    Update settings for Turkish localization, enhance static file handling, and improve course template structure

--- coursapp/settings.py
+++ coursapp/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'tr'
 
 TIME_ZONE = 'UTC'
 
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.template import TemplateDoesNotExist
@@ -16,25 +16,25 @@ db = {
         {             
             "title": "javascript kursu", 
             "description": "javascript kurs açıklaması", 
-            "iamgeUrl": "https://img-c.udemycdn.com/course/750x422/1662526_fc1c_3.jpg",
+            "imageUrl": "1.jpg",
             "slug": "javascript-kursu",
-            "date": date(2025, 10, 10),
+            "date": datetime.now(),
             "isActive": True,
             "isUpdated": False
         },
         {             
             "title": "python kursu", 
             "description": "python kurs açıklaması", 
-            "iamgeUrl": "https://img-c.udemycdn.com/course/750x422/2463492_8344_3.jpg",
+            "imageUrl": "2.jpg",
             "slug": "python-kursu",
             "date": date(2025, 9, 10),
-            "isActive": False,
+            "isActive": True,
             "isUpdated": False
         },
         {             
             "title": "web geliştirme kursu", 
             "description": "web geliştirme kurs açıklaması", 
-            "iamgeUrl": "https://img-c.udemycdn.com/course/750x422/1258436_2dc3_4.jpg",
+            "imageUrl": "3.jpg",
             "slug": "web-gelistirme-kursu",
             "date": date(2025, 8, 10),
             "isActive": True,
